e360b2a4d12ce69b0cb023c147e166190eb70727
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-07-18 15:07:29 -0500

    Rename run.py to ru.py

---
+++ qwerty_cli/ru.py
@@ -0,0 +1,26 @@
+import math
+import string
+import sqlite3 as db
+# import data_types
+# import data_errors
+import os
+
+"""
+@TODO: populate files:
+* /classes/data types.py
+* /classes/data_errors.py
+* /classes/data_commands.py
+* /classes/data_variables.py
+* /classes/data_storage/data_reader.py
+* /classes/data_storage/data_writer.py
+* /classes/data_storage/data_creator.py
+* /classes/data_storage/data/vars.db
+* /classes/data_storage/data/history.db
+* /classes/data_storage/data/custom/types.db
+* /classes/data_storage/data/custom/vars.db
+* /classes/data_storage/data/custom/commands.db
+* /classes/data_storage/data/users/local_admin/actions.db
+* /classes/data_storage/data/users/global_admin/actions.db
+* /run.py
+
+"""
